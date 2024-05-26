124ec0e81d8bf052fd1e2e33e35b4fda6130843c
muru <ehdguschkf77@naver.com>
2024-05-26 19:34:38 +0900

    삭제

--- 샘플데이터/sample_html/Main/MainPage.js
+++
@@ -1,7 +0,0 @@
-const inputField = document.querySelector('input[type="text"]');
-
-// 클릭 이벤트 리스너 추가
-inputField.addEventListener('click', () => {
-    inputField.value = '';   // 입력 필드 내용 지우기
-    inputField.focus();     // 입력 필드에 포커스 설정
-});
